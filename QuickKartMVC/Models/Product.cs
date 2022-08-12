29be9c983d2f6b0e6b7e81123d66467e34b8459b
Pavan Manjunath <pavanbm17@gmail.com>
2022-08-12 19:56:29 +0530

    12th aug connecting MVC to WEB API

---
+++ QuickKartMVC/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuickKartMVC.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<byte> CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityAvailable { get; set; }
+    }
+
+}
